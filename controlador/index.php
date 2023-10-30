0540cc1f6e000d98dfca53440173f18e772facf1
AlbeX <a.morcillo@sapalomera.cat>
2023-10-30 16:35:31 +0100

    Añadir cosas en index.php + validaciones en conection.php

--- controlador/index.php
+++ controlador/index.php
@@ -2,16 +2,23 @@
 
 $errors = '';
 
-$validNombre = isset($_POST['nombre']) ? htmlspecialchars($_POST['nombre']) : '';
-$validApellido = isset($_POST['apellido']) ? htmlspecialchars($_POST['apellido']) : '';
+$validChoice1 = isset($_POST['choice1']) ? htmlspecialchars($_POST['coice1']) : '';
+$validChoice2 = isset($_POST['choice2']) ? htmlspecialchars($_POST['choice2']) : '';
+$validNombre = isset($_POST['nom']) ? htmlspecialchars($_POST['nom']) : '';
+$validApellido = isset($_POST['cognoms']) ? htmlspecialchars($_POST['cognoms']) : '';
 $validEmail = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
-$validTelf = isset($_POST['telefono']) ? htmlspecialchars($_POST['telefono']) : '';
-$validGenero = isset($_POST['genero']) ? htmlspecialchars($_POST['genero']) : '';
+$validTelf = isset($_POST['telefon']) ? htmlspecialchars($_POST['telefon']) : '';
+$validGenero = isset($_POST['genere']) ? htmlspecialchars($_POST['genere']) : '';
+$validPersonas = isset($_POST['persones']) ? htmlspecialchars($_POST['persones']) : '';
+$validDescuento = isset($_POST['descompte']) ? htmlspecialchars($_POSRT['descompte']) : '';
+$validPreu = isset($_POST['preu']) ? htmlspecialchars($_POST['preu']) : '';
 
 require_once '../modelo/conection.php';
 
-function comprobarNombreOk(){
-
-}
+validarNombreOk($validNombre, $errors);
+validarApellidoOk($validApellido, $errors);
+validarEmailOk($validEmail, $errors);
+validarTelfOk($validTelf, $errors);
+validarGeneroOk($validGenero, $errors);
 
 ?>
