d199e302d052e532e3a54f1bdb6d5ed6c240c7c2
Benito Martinez Florido <99820356+Benito44@users.noreply.github.com>
2023-10-30 16:16:45 +0100

    Sel·lecció de continents i paissos i preu desactivat

--- DataHora.js
+++ DataHora.js
@@ -1,5 +1,23 @@
 "use strict";
+function filter_options(){
+	if (typeof $("#choice1").data('options') === "undefined") {
+       $("#choice1").data('options', $('#choice2 option').clone());
+  }
+    var id = $("#choice1").val();
+    var options = $("#choice1").data('options').filter('[data-option=' + id + ']');
+    $('#choice2').html(options);
+}
 
+$(function () {
+		//Ejecutar el filtro la primera vez
+		filter_options();
+    
+    //actualizar al cambiar el factor
+    $("#choice1").change(function () {
+	    filter_options();
+		});
+    
+});
 /******************************************************
 * EN AQUEST APARTAT PODEU AFEGIR O MODIFICAR FUNCIONS *
 ******************************************************/
@@ -204,3 +222,4 @@ function init() {
 	clock();
 	setInterval(clock, 1000);
 }
+window.onload = init;
