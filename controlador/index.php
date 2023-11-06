0bbbc3453e2087b425b8e5f51fc826d21377885a
BenitoMF <b.martinez2@sapalomera.cat>
2023-11-06 17:03:48 +0100

    validacions

--- controlador/index.php
+++ controlador/index.php
@@ -1,3 +1,18 @@
+<html>
+<head>
+		<title>WONDERFULL TRAVELL</title>
+		<meta charset="UTF-8" />
+		<style>
+			body{font-family:Arial,Helvetica,sans-serif;}
+			div.datahora{font-size:1.2em; font-weight:bold; color:white; text-align:center; border-radius:0.9em; background-image:linear-gradient(#cebe27, #45ab76); width:9em; padding:0.4em 0;}
+			iframe{border:1px solid black; width:1000px; height: 500px;}
+		</style>
+		    <link rel="stylesheet" href="estils.css"> <!-- Hacer referencia a tu archivo de estilos -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script defer src="../vista/DataHora.js"></script>
+	</head>
+</html>
+
 <?php
 
 $errors = '';
@@ -9,8 +24,8 @@ $validApellido = isset($_POST['cognoms']) ? htmlspecialchars($_POST['cognoms']) 
 $validEmail = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
 $validTelf = isset($_POST['telefon']) ? htmlspecialchars($_POST['telefon']) : '';
 $validGenero = isset($_POST['genere']) ? htmlspecialchars($_POST['genere']) : '';
-$validPersonas = isset($_POST['persones']) ? htmlspecialchars($_POST['persones']) : '';
-$validDescuento = isset($_POST['descompte']) ? htmlspecialchars($_POSRT['descompte']) : '';
+$validPersonas = isset($_POST['persones']) ? htmlspecialchars($_POST['persones']) : 1;
+$validDescuento = isset($_POST['descompte']) ? htmlspecialchars($_POST['descompte']) : '';
 $validPreu = isset($_POST['preu']) ? htmlspecialchars($_POST['preu']) : '';
 
 require_once '../modelo/conection.php';
@@ -20,5 +35,5 @@ validarApellidoOk($validApellido, $errors);
 validarEmailOk($validEmail, $errors);
 validarTelfOk($validTelf, $errors);
 validarGeneroOk($validGenero, $errors);
-
+include '../vista/index_view.php';
 ?>
--- modelo/conection.php
+++ modelo/conection.php
@@ -1,16 +1,16 @@
 <?php
 
-function validarNombreOk($validNombre, $errors){
+function validarNombreOk($validNombre, &$errors){
     if (empty($validNombre)) {
-        $errors .= "El nombre está vacío.</br>";
+         $errors .= "El nombre está vacío.</br>";
     }
      // Verificar si el nombre contiene caracteres válidos
      if (!preg_match("/^([a-zA-Z']+)$/", $validNombre)) {
-        $errors .= "Nombre no válido. Solo se permiten letras y apóstrofos.</br>";
+     $errors .= "Nombre no válido. Solo se permiten letras y apóstrofos.</br>";
     }
 }
 
-function validarApellidoOk($validApellido, $errors){
+function validarApellidoOk($validApellido, &$errors){
     if (empty($validApellido)) {
         $errors .= "El apellido está vacío.</br>";
     }
@@ -20,7 +20,7 @@ function validarApellidoOk($validApellido, $errors){
     }
 }
 
-function validarEmailOk($validEmail, $errors){
+function validarEmailOk($validEmail, &$errors){
     if (empty($email)) {
         $errors .= 'El correo electrónico está vacío.</br>';
     }
@@ -31,7 +31,7 @@ function validarEmailOk($validEmail, $errors){
     }
 }
 
-function validarTelfOk($validTelf, $errors){
+function validarTelfOk($validTelf, &$errors){
     if (empty($validTelf)){
         $errors .= 'El teléfono no puede estar vacío.<br>';
     }
@@ -40,7 +40,7 @@ function validarTelfOk($validTelf, $errors){
     }
 }
 
-function validarGeneroOk($validGenero, $errors){
+function validarGeneroOk($validGenero, &$errors){
     if (empty($validGenero)){
         $errors .= 'El género no puede estar vacío.<br>';
     }
