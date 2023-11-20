46e3ea5b421716f1a132973703a433711441bb40
BenitoMF <b.martinez2@sapalomera.cat>
2023-11-20 17:01:33 +0100

    Rellotge analògic v2

--- vista/index_view.php
+++ vista/index_view.php
@@ -4,17 +4,55 @@
 		<title>WONDERFULL TRAVELL</title>
 		<meta charset="UTF-8" />
 		<style>
+			.clock {
+    margin: 0px auto;
+    width: 650px;
+    height: 650px;
+    fill: black
+  }
+  .outer-circle,
+  .center-circle {
+    fill: DarkSlateGray;
+  }
+  
+  .hours,
+  .minutes,
+  .seconds {
+    transform-origin: center;
+    stroke-linecap: round;
+  }
+  
+  .hours {
+    stroke: fuchsia;
+    stroke-width: 3px; 
+    transition: transform 1s ease-in-out;
+  }
+  .minutes {
+    stroke-width: 2px; 
+    stroke: lime;
+    transition: transform 1s ease-in-out;
+  }
+  .seconds {
+    stroke: white;
+  }
+  
+  .line {
+    stroke-width: 1px;
+    stroke: white;
+    stroke-linecap: round;
+    transform-origin: center;
+  }
 		</style>
 		    <link rel="stylesheet" href="../estilos/style.css"> <!-- Hacer referencia a tu archivo de estilos -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 		<script src="./calculos.js"></script>
-		<script defer src="DataHora.js"></script>
+		<!-- <script defer src="DataHora.js"></script>-->
+		<script  src="./script.js"></script>
 	</head>
 	<body>
-		<form method="post" action="../controlador/index.php">
-		<div class="clock">
+	<div class="clock">
   <svg class="circle" viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="60" cy="60" r="60" class="outer-circle"/>
+    <circle cx="60" cy="60" r="60" class="outer-circle" />
     <circle cx="60" cy="60" r="57" />
     <line x1="60" y1="20" x2="60" y2="60" class="hours" />
     <line x1="60" y1="2" x2="60" y2="60" class="minutes" />
@@ -34,6 +72,9 @@
     <line x1="60" y1="5" x2="60" y2="10" class="line" />
   </svg>
 </div>
+<!-- partial -->
+		<form method="post" action="../controlador/index.php">
+
 		Data <input type="date" id="datahora" name="datahora" ><br>
 		Desti 		
 		<select class="form-control" id="choice1" name="choice1">
