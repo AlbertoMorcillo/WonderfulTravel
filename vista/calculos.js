d9250cf2e815f09f7b81836a0ff1f2420806e4fe
AlbeX <a.morcillo@sapalomera.cat>
2023-11-20 17:01:11 +0100

    calculos js

--- vista/calculos.js
+++ vista/calculos.js
@@ -1,1 +1,40 @@
 'use strict'
+
+const AFGANISTAN_PRECIO = 3523;
+const JAPON_PRECIO = 1231;
+const YEMEN_PRECIO = 1849;
+const ESPAÑA_PRECIO = 86;
+const PORTUGAL_PRECIO = 93;
+const LUXEMBURGO_PRECIO = 113;
+const UGANDA_PRECIO = 912;
+const SIERRA_LEONA_PRECIO = 2382;
+const MARRUECOS_PRECIO = 51;
+const MEXICO_PRECIO = 971;
+const BRASIL_PRECIO = 856;
+const ARGENTINA_PRECIO = 1075;
+
+
+  // Función para calcular el precio
+  function calcularPrecio() {
+    let pais = document.getElementById('choice2').value;
+    let personas = parseInt(document.getElementById('persones').value);
+    let descuento = document.getElementById('descompte').checked;
+
+    let precioBase = 100; // Precio base de referencia
+
+    if (descuento) {
+      precioBase -= 10; // Reducción de $10 si se aplica el descuento
+    }
+
+    let precioTotal = precioBase * personas;
+
+    document.getElementById('preu').value = precioTotal;
+  }
+
+  // Event listeners para detectar cambios en los campos relevantes
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('choice2').addEventListener('change', calcularPrecio);
+    document.getElementById('persones').addEventListener('input', calcularPrecio);
+    document.getElementById('descompte').addEventListener('change', calcularPrecio);
+  });
+
