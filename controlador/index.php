6081e19c095828749be1480eb057745f447dce93
AlbeX <a.morcillo@sapalomera.cat>
2023-11-24 15:43:39 +0100

    Arrego formato html del index.php

--- controlador/index.php
+++ controlador/index.php
@@ -1,83 +1,108 @@
-<html>
-<head>
-		<title>WONDERFULL TRAVELL</title>
-		<meta charset="UTF-8" />
-		<style>
-			.clock {
-    margin: 0px auto;
-    width: 650px;
-    height: 650px;
-    fill: black
-  }
-  .outer-circle,
-  .center-circle {
-    fill: DarkSlateGray;
-  }
-  
-  .hours,
-  .minutes,
-  .seconds {
-    transform-origin: center;
-    stroke-linecap: round;
-  }
-  
-  .hours {
-    stroke: fuchsia;
-    stroke-width: 3px; 
-    transition: transform 1s ease-in-out;
-  }
-  .minutes {
-    stroke-width: 2px; 
-    stroke: lime;
-    transition: transform 1s ease-in-out;
-  }
-  .seconds {
-    stroke: white;
-  }
-  
-  .line {
-    stroke-width: 1px;
-    stroke: white;
-    stroke-linecap: round;
-    transform-origin: center;
-  }
-			body{font-family:Arial,Helvetica,sans-serif;}
-			div.datahora{font-size:1.2em; font-weight:bold; color:white; text-align:center; border-radius:0.9em; background-image:linear-gradient(#cebe27, #45ab76); width:9em; padding:0.4em 0;}
-			iframe{border:1px solid black; width:1000px; height: 500px;}
-		</style>
-		    <link rel="stylesheet" href="../estilos/style.css"> <!-- Hacer referencia a tu archivo de estilos -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script defer src="../vista/script.js"></script>
-		<script defer src="../vista/calculos.js"></script>
-		<script defer src="../vista/DataHora.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>WONDERFULL TRAVELL</title>
+	<meta charset="UTF-8" />
+	<style>
+		.clock {
+			margin: 0px auto;
+			width: 650px;
+			height: 650px;
+			fill: black
+		}
+
+		.outer-circle,
+		.center-circle {
+			fill: DarkSlateGray;
+		}
+
+		.hours,
+		.minutes,
+		.seconds {
+			transform-origin: center;
+			stroke-linecap: round;
+		}
+
+		.hours {
+			stroke: fuchsia;
+			stroke-width: 3px;
+			transition: transform 1s ease-in-out;
+		}
+
+		.minutes {
+			stroke-width: 2px;
+			stroke: lime;
+			transition: transform 1s ease-in-out;
+		}
+
+		.seconds {
+			stroke: white;
+		}
+
+		.line {
+			stroke-width: 1px;
+			stroke: white;
+			stroke-linecap: round;
+			transform-origin: center;
+		}
+
+		body {
+			font-family: Arial, Helvetica, sans-serif;
+		}
+
+		div.datahora {
+			font-size: 1.2em;
+			font-weight: bold;
+			color: white;
+			text-align: center;
+			border-radius: 0.9em;
+			background-image: linear-gradient(#cebe27, #45ab76);
+			width: 9em;
+			padding: 0.4em 0;
+		}
+
+		iframe {
+			border: 1px solid black;
+			width: 1000px;
+			height: 500px;
+		}
+	</style>
+	<link rel="stylesheet" href="../estilos/style.css"> <!-- Hacer referencia a tu archivo de estilos -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script defer src="../vista/script.js"></script>
+	<script defer src="../vista/calculos.js"></script>
+	<script defer src="../vista/DataHora.js"></script>
 
 	</head>
+
 	<body>
 		<!-- partial -->
-<script  src="./script.js"></script>
-	<div class="clock">
-  <svg class="circle" viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="60" cy="60" r="60" class="outer-circle" />
-    <circle cx="60" cy="60" r="57" />
-    <line x1="60" y1="20" x2="60" y2="60" class="hours" />
-    <line x1="60" y1="2" x2="60" y2="60" class="minutes" />
-    <line x1="60" y1="0" x2="60" y2="60" class="seconds" />
-    <circle cx="60" cy="60" r="3" class="center-circle" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-    <line x1="60" y1="5" x2="60" y2="10" class="line" />
-  </svg>
-</div>
+		<script src="./script.js"></script>
+		<div class="clock">
+			<svg class="circle" viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">
+				<circle cx="60" cy="60" r="60" class="outer-circle" />
+				<circle cx="60" cy="60" r="57" />
+				<line x1="60" y1="20" x2="60" y2="60" class="hours" />
+				<line x1="60" y1="2" x2="60" y2="60" class="minutes" />
+				<line x1="60" y1="0" x2="60" y2="60" class="seconds" />
+				<circle cx="60" cy="60" r="3" class="center-circle" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+				<line x1="60" y1="5" x2="60" y2="10" class="line" />
+			</svg>
+		</div>
 	</body>
+
 </html>
 
 <?php
@@ -86,7 +111,7 @@ session_start();
 
 // Verificar si la sesión de viajes no está inicializada
 if (!isset($_SESSION['viajes'])) {
-    $_SESSION['viajes'] = array(); // Inicializarla como un arreglo vacío
+	$_SESSION['viajes'] = array(); // Inicializarla como un arreglo vacío
 }
 
 $errors = '';
@@ -108,18 +133,18 @@ $validDescuento = isset($_POST['descompte']) ? htmlspecialchars($_POST['descompt
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
 
-require 'Validaciones.php';
+	require 'Validaciones.php';
 
-validarNombreOk($validNombre, $errors);
-validarApellidoOk($validApellido, $errors);
-validarEmailOk($validEmail, $errors);
-validarTelfOk($validTelf, $errors);
-validarGeneroOk($validGenero, $errors);
-$dataViatge = $valor;
-$fecha1 = new DateTime($dataViatge);
-$fecha2 = new DateTime();
+	validarNombreOk($validNombre, $errors);
+	validarApellidoOk($validApellido, $errors);
+	validarEmailOk($validEmail, $errors);
+	validarTelfOk($validTelf, $errors);
+	validarGeneroOk($validGenero, $errors);
+	$dataViatge = $valor;
+	$fecha1 = new DateTime($dataViatge);
+	$fecha2 = new DateTime();
 
-/**if (empty($errors)){
+	/**if (empty($errors)){
 	try {
 		// Establecer la conexión a la base de datos
 		$connexio = new PDO('mysql:host=localhost;dbname=wonderfull_travel', 'root', '');
@@ -139,75 +164,75 @@ $fecha2 = new DateTime();
 	}
 	}*/
 	if (!isset($_SESSION['viaje_insertado'])) {
-	if (empty($errors)){
-		try {
-			$preu = 1;
-			$dataViatge = $valor;
-			/**
-			 * $fecha1 = new DateTime($dataViatge);
-			*  $fecha2 = new DateTime();
+		if (empty($errors)) {
+			try {
+				$preu = 1;
+				$dataViatge = $valor;
+				/**
+				 * $fecha1 = new DateTime($dataViatge);
+				 *  $fecha2 = new DateTime();
 
-			*  $diferencia = $fecha1->diff($fecha2);
+				 *  $diferencia = $fecha1->diff($fecha2);
 			
-			*  $data = $diferencia->format("%a");
-			*  if ($data == 0){
-			* 	 $data = 1;
-			* }
-			* echo "La diferencia en noches es: " . $data . " noches";
-			 */
+				 *  $data = $diferencia->format("%a");
+				 *  if ($data == 0){
+				 * 	 $data = 1;
+				 * }
+				 * echo "La diferencia en noches es: " . $data . " noches";
+				 */
 
-			// Establecer la conexión a la base de datos
+				// Establecer la conexión a la base de datos
 
-			$connexio = new PDO('mysql:host=localhost;dbname=wonderfull_travel', 'root', '');
-			$statement = $connexio->prepare("SELECT preu FROM paisos WHERE nombre = ?");
-			$statement->bindParam(1,$validChoice2);
-			$statement->execute();
-			while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-				$preu = $row["preu"];
+				$connexio = new PDO('mysql:host=localhost;dbname=wonderfull_travel', 'root', '');
+				$statement = $connexio->prepare("SELECT preu FROM paisos WHERE nombre = ?");
+				$statement->bindParam(1, $validChoice2);
+				$statement->execute();
+				while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+					$preu = $row["preu"];
+				}
+
+				$preu = $preu * $validPersonas;
+				$statement = $connexio->prepare("INSERT INTO viatges (destí, preu_total, num_persones, data, pais) VALUES (?,?,?,?,?)");
+				$statement->bindParam(1, $validChoice1);
+				$statement->bindParam(2, $preu);
+				$statement->bindParam(3, $validPersonas);
+				$statement->bindParam(4, $dataViatge);
+				$statement->bindParam(5, $validChoice2);
+				$statement->execute();
+
+
+
+				// Limitar la visualización a tres registros
+				$_SESSION['viajes'][] = array(
+					'Destino' => $validChoice1,
+					'Precio total' => $preu,
+					'Número de personas' => $validPersonas,
+					'Fecha' => $dataViatge,
+					'País' => $validChoice2
+				);
+
+				// Mostrar la información de viajes anteriores almacenada en la sesión
+				$totalViajes = count($_SESSION['viajes']);
+				$inicio = $totalViajes > 3 ? $totalViajes - 3 : 0;
+
+				for ($i = $inicio; $i < $totalViajes; $i++) {
+					$viaje = $_SESSION['viajes'][$i];
+					echo "Destino: " . $viaje['Destino'] . "<br>";
+					echo "Precio total: " . $viaje['Precio total'] . " €" . "<br>";
+					echo "Número de personas: " . $viaje['Número de personas'] . "<br>";
+					echo "Fecha: " . $viaje['Fecha'] . "<br>";
+					echo "País: " . $viaje['País'] . "<br>";
+					echo "<br>";
+				}
+				// Establecer el modo de errores para PDO
+				$connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			} catch (PDOException $e) {
+				// Manejar errores de conexión
+				echo "Error de conexión a la base de datos: " . $e->getMessage();
+				die();
 			}
-
-			$preu = $preu * $validPersonas;
-			$statement = $connexio->prepare("INSERT INTO viatges (destí, preu_total, num_persones, data, pais) VALUES (?,?,?,?,?)");
-			$statement->bindParam(1,$validChoice1);
-			$statement->bindParam(2,$preu);
-			$statement->bindParam(3,$validPersonas);
-			$statement->bindParam(4,$dataViatge);
-			$statement->bindParam(5,$validChoice2);
-			$statement->execute();
-
-		
-		   
-					   // Limitar la visualización a tres registros
-					   $_SESSION['viajes'][] = array(
-						   'Destino' => $validChoice1,
-						   'Precio total' => $preu,
-						   'Número de personas' => $validPersonas,
-						   'Fecha' => $dataViatge,
-						   'País' => $validChoice2
-					   );
-		   
-					   // Mostrar la información de viajes anteriores almacenada en la sesión
-					   $totalViajes = count($_SESSION['viajes']);
-					   $inicio = $totalViajes > 3 ? $totalViajes - 3 : 0;
-		   
-					   for ($i = $inicio; $i < $totalViajes; $i++) {
-						   $viaje = $_SESSION['viajes'][$i];
-						   echo "Destino: " . $viaje['Destino'] . "<br>";
-						   echo "Precio total: " . $viaje['Precio total'] . " €" . "<br>";
-						   echo "Número de personas: " . $viaje['Número de personas'] . "<br>";
-						   echo "Fecha: " . $viaje['Fecha'] . "<br>";
-						   echo "País: " . $viaje['País'] . "<br>";
-						   echo "<br>";
-					   }
-            // Establecer el modo de errores para PDO
-            $connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);		} catch(PDOException $e) {
-			// Manejar errores de conexión
-			echo "Error de conexión a la base de datos: " . $e->getMessage();
-			die();
 		}
-		}
-
-}
+	}
 }
 include '../vista/index_view.php';
 ?>
