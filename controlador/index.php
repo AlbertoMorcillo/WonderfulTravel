3f4f222ffc11aaa03ef5f3a17ab27bc996323b24
BenitoMF <b.martinez2@sapalomera.cat>
2023-11-27 00:51:43 +0100

    Enviar mail al reservar el viatge

--- controlador/index.php
+++ controlador/index.php
@@ -49,6 +49,12 @@ session_start();
 if (!isset($_SESSION['viajes'])) {
     $_SESSION['viajes'] = array(); // Inicializarla como un arreglo vacío
 }
+require '../src/PHPMailer.php';
+require '../src/Exception.php';
+require '../src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 $errors = '';
 $viatges = '';
@@ -129,6 +135,37 @@ if (empty($errors)){
 		$statement->bindParam(4,$validEmail);
 		$statement->bindParam(5,$validGenero);
 		$statement->execute();
+
+		$mail = new PHPMailer(true);
+		$nom = $validNombre; $adreca = $validEmail;
+		// Canviar les opcions del SMTP
+		$mail->SMTPDebug =0;
+		$mail->SMTPOptions = array(
+		  'ssl' => array(
+			  'verify_peer' => false,
+			  'verify_peer_name' => false,
+			  'allow_self_signed' => true
+		  )
+	  );                      
+		$mail->isSMTP();                                            //Enviar utilitzant SMTP
+		$mail->Host       = 'smtp.gmail.com';                    
+		$mail->SMTPAuth   = true;                                   //Activem l'autenticació SMTP
+		$mail->Username   = 'xamppbmartinez@gmail.com';                     //Email on creem la clau
+		$mail->Password   = 'jvrg fwih oxgm ncwm';                          //Clau d'acces
+		$mail->SMTPSecure = 'PHPMailer::ENCRYPTION_STARTTLS';            //Enable implicit TLS encryption
+		$mail->Port       = 587;                                    // Utilitzem el port 587
+	  
+		//Recipients
+		$mail->setFrom('xamppbmartinez@gmail.com', $nom); 
+		$mail->addAddress($adreca);     
+	  
+		//Content
+		$mail->isHTML(true); //Enviar l'email en format HTML
+		$mail->Subject = 'Reserves fetes'; // Assumpte
+		$mail->Body    = "Hola Alberto te hablo desde la pàgina web ";  
+	  
+		$mail->send(); // Enviem l'email
+		
 		// Establecer el modo de errores para PDO
 		$connexio->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch(PDOException $e) {
